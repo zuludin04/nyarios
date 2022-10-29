2a09245140a68730169f17f391cb66e7fbb98a93
zuludin <zmauludin@bwa.id>
2022-10-29 10:50:35 +0700

    change file color and spacing

--- lib/ui/chat/widgets/chat_item.dart
+++ lib/ui/chat/widgets/chat_item.dart
@@ -85,6 +85,7 @@ class ChatItem extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   _showChatType(chat.type!),
+                  const SizedBox(height: 4),
                   Row(
                     mainAxisSize: MainAxisSize.min,
                     children: [
@@ -139,7 +140,7 @@ class ChatItem extends StatelessWidget {
       case 'file':
         return Container(
           decoration: BoxDecoration(
-            color: const Color(0xffb3404a),
+            color: Colors.red.withOpacity(0.3),
             borderRadius: BorderRadius.circular(10),
           ),
           padding: const EdgeInsets.all(12),
