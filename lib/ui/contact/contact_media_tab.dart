2f0966960651df3ffe3fd95614c99068e49f3fc7
zuludin <zmauludin@bwa.id>
2022-10-29 11:30:29 +0700

    widget for different file type

--- lib/ui/contact/contact_media_tab.dart
+++ lib/ui/contact/contact_media_tab.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 import '../../core/widgets/custom_indicator.dart';
+import '../../data/model/chat.dart';
 import 'contact_media_controller.dart';
 
 class ContactMediaTab extends StatefulWidget {
@@ -38,13 +40,78 @@ class _ContactMediaTabState extends State<ContactMediaTab>
           return const Center(child: Text("Empty Media"));
         } else {
           return ListView.builder(
-            itemBuilder: (context, index) => ListTile(
-              title: Text(controller.mediaChats[index].message ?? ""),
-            ),
+            itemBuilder: (context, index) {
+              var chat = controller.mediaChats[index];
+              if (widget.type == "file") {
+                return _buildDocItem(chat);
+              } else {
+                return _buildMediaItem(chat);
+              }
+            },
             itemCount: controller.mediaChats.length,
           );
         }
       },
+    );
+  }
+
+  Widget _buildDocItem(Chat chat) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(12),
+          child: Row(
+            children: [
+              const Icon(Icons.attach_file),
+              const SizedBox(width: 8),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    chat.message ?? "",
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    DateFormat("dd/MM/yyyy").format(
+                      DateTime.fromMillisecondsSinceEpoch(
+                          chat.sendDatetime ?? 0),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        const Divider(),
+      ],
+    );
+  }
+
+  Widget _buildMediaItem(Chat chat) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Image.network(
+                chat.url ?? "",
+                width: double.infinity,
+                height: 250,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                DateFormat("dd/MM/yyyy").format(
+                    DateTime.fromMillisecondsSinceEpoch(
+                        chat.sendDatetime ?? 0)),
+              )
+            ],
+          ),
+        ),
+        const Divider(),
+      ],
     );
   }
 }
