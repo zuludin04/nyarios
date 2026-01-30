8df57c951210d878b2cc86b44a008d475066c1b5
zuludin <zmauludin04@gmail.com>
2026-01-30 20:50:26 +0700

    fix: remove notification setting in home screen

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,18 +1,9 @@
-import 'package:another_flushbar/flushbar.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 import 'package:nyarios/core/widgets/bottom_navigation.dart';
 import 'package:nyarios/core/widgets/image_asset.dart';
-import 'package:nyarios/data/model/call.dart';
-import 'package:nyarios/data/model/contact.dart';
-import 'package:nyarios/data/model/notification.dart' as notif;
-import 'package:nyarios/data/repositories/call_repository.dart';
-import 'package:nyarios/domain/providers/repository_providers.dart';
 import 'package:nyarios/routes/app_pages.dart';
-import 'package:nyarios/services/storage_services.dart';
 import 'package:nyarios/ui/home/call_history/call_history_screen.dart';
 import 'package:nyarios/ui/home/recent_chat/recent_chat_screen.dart';
 import 'package:nyarios/ui/home/settings/settings_screen.dart';
@@ -25,15 +16,7 @@ class HomeScreen extends ConsumerStatefulWidget {
 }
 
 class _HomeScreenState extends ConsumerState<HomeScreen> {
-  late Stream<DocumentSnapshot<Map<String, dynamic>>> notifStream;
-
   int selectedNav = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    listenFirebaseNotification(ref.read(callRepositoryProvider));
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -86,196 +69,5 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
         ],
       ),
     );
-  }
-
-  void listenFirebaseNotification(CallRepository callRepo) {
-    notifStream = FirebaseFirestore.instance
-        .collection('notification')
-        .doc(StorageServices.to.userId)
-        .snapshots();
-
-    notifStream.listen((event) {
-      if (event.exists) {
-        var notification = notif.Notification.fromMap(event.data()!);
-        showCallNotification(notification, callRepo);
-      }
-    });
-  }
-
-  void showCallNotification(
-    notif.Notification notification,
-    CallRepository callRepo,
-  ) {
-    Flushbar(
-      boxShadows: [
-        BoxShadow(
-          color: Colors.grey.shade400,
-          blurRadius: 1,
-          spreadRadius: 1,
-          offset: const Offset(1, 1),
-          blurStyle: BlurStyle.outer,
-        ),
-      ],
-      borderRadius: BorderRadius.circular(16),
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      margin: const EdgeInsets.all(16),
-      flushbarPosition: FlushbarPosition.TOP,
-      flushbarStyle: FlushbarStyle.FLOATING,
-      messageText: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                decoration: const BoxDecoration(
-                  color: Color(0xffb3404a),
-                  shape: BoxShape.circle,
-                ),
-                padding: const EdgeInsets.all(8),
-                child: ImageAsset(
-                  assets: notification.type == 'video_call'
-                      ? 'assets/icons/ic_video.png'
-                      : 'assets/icons/ic_call.png',
-                  size: 18,
-                ),
-              ),
-              const SizedBox(width: 16),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          notification.profile!.name!,
-                          style: const TextStyle(fontWeight: FontWeight.w500),
-                        ),
-                        const SizedBox(width: 8),
-                        Text(
-                          DateFormat("HH:mm a").format(
-                            DateTime.fromMillisecondsSinceEpoch(
-                              notification.callingTime!,
-                            ),
-                          ),
-                          style: const TextStyle(
-                            fontSize: 13,
-                            fontWeight: FontWeight.w100,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Text(
-                      notification.type == 'video_call'
-                          ? 'Incoming video call'
-                          : 'Incoming voice call',
-                    ),
-                  ],
-                ),
-              ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(18),
-                child: Image.network(
-                  notification.profile!.photo!,
-                  width: 34,
-                  height: 34,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 8),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    saveCallHistory(
-                      notification.callId!,
-                      notification.profile!.uid!,
-                      notification.type!,
-                      false,
-                      callRepo,
-                    );
-                    updateCallingStatus(
-                      notification.profile!.uid!,
-                      notification.callId!,
-                      callRepo,
-                    );
-                    FirebaseFirestore.instance
-                        .collection('notification')
-                        .doc(StorageServices.to.userId)
-                        .delete();
-                    Get.back();
-                  },
-                  child: const Text(
-                    'Decline',
-                    style: TextStyle(color: Colors.red),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    saveCallHistory(
-                      notification.callId!,
-                      notification.profile!.uid!,
-                      notification.type!,
-                      true,
-                      callRepo,
-                    );
-                    FirebaseFirestore.instance
-                        .collection('notification')
-                        .doc(StorageServices.to.userId)
-                        .delete();
-                    Get.back();
-                    Get.toNamed(
-                      notification.type == 'voice_call'
-                          ? AppRoutes.callVoice
-                          : AppRoutes.callVideo,
-                      arguments: Contact(
-                        profile: notification.profile,
-                        chatId: notification.chatId,
-                      ),
-                    );
-                  },
-                  child: const Text(
-                    'Answer',
-                    style: TextStyle(color: Colors.green),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-      duration: const Duration(seconds: 100),
-    ).show(context);
-  }
-
-  void saveCallHistory(
-    String callId,
-    String profileId,
-    String type,
-    bool isAccepted,
-    CallRepository callRepo,
-  ) async {
-    var call = Call(
-      callDate: DateTime.now().millisecondsSinceEpoch,
-      callId: callId,
-      profileId: profileId,
-      status: 'incoming_call',
-      type: type,
-      isAccepted: isAccepted,
-    );
-
-    callRepo.saveCallHistory(StorageServices.to.userId, call);
-  }
-
-  void updateCallingStatus(
-    String profileId,
-    String callId,
-    CallRepository callRepo,
-  ) async {
-    callRepo.updateCallStatus(profileId, callId, false);
   }
 }
